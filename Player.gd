969d8b9bf71019364c3b115fdc233aa01df2d0bd
Andrea Domenico Giuliano <draconian892@gmail.com>
2021-12-06 00:15:20 +0100

    Add Player hit signal and start function

--- Player.gd
+++ Player.gd
@@ -4,30 +4,28 @@ extends Area2D
 export var speed = 400;
 # Size of the game window.
 var screen_size;
+# Collision hit signal
+signal hit;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size;
+	# Hide the player at the beginning of the game
 	hide();
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	# Vector2 is the player moement vecot
 	var velocity = Vector2();
-	
+	# Input management
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1;
-		
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1;
-	
 	if Input.is_action_pressed("ui_down"):
 		velocity.y += 1;
-		
 	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1;
-		
 	if velocity.length() > 0:
 		# To avoid fast diagonal movement.
 		velocity = velocity.normalized() * speed;
@@ -36,14 +34,11 @@ func _process(delta):
 	else:
 		# $ return the rispective node object
 		$PlayerAnimatedSprite.stop();
-	
 	# Update the player position
 	position += velocity * delta;
-	
 	# Clamp prevent the player from leaving the screen
 	position.x = clamp(position.x, 0, screen_size.x);
 	position.y = clamp(position.y, 0, screen_size.y);
-	
 	# Change the animation in base of the vector direction
 	if velocity.x != 0:
 		$PlayerAnimatedSprite.animation = "walk";
@@ -52,5 +47,18 @@ func _process(delta):
 	elif velocity.y != 0:
 		$PlayerAnimatedSprite.animation = "up";
 		$PlayerAnimatedSprite.flip_v = velocity.y > 0;
-	
-	
+
+# Called when the player is hitted by an enemy
+func _on_Player_body_entered(body):
+	hide();
+	emit_signal("hit");
+	$PlayerCollisionShape2D.set_deferred("disabled", true);
+
+# Function called at the start of the game
+func start(pos):
+	position = pos;
+	show();
+	$PlayerCollisionShape2D.disabled = false;
+
+
+
