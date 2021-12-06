ed93059a169fde5e8794d9c02b4ba32450db5ea7
Andrea Domenico Giuliano <draconian892@gmail.com>
2021-12-07 00:12:09 +0100

    Add WASD Support

--- Player.gd
+++ Player.gd
@@ -13,18 +13,34 @@ func _ready():
 	# Hide the player at the beginning of the game
 	hide();
 
+# Check if is pressed the right arrow or D
+func IsPressedRight(Input):
+	return (Input.is_action_pressed("ui_right") || Input.is_key_pressed(KEY_D));
+
+# Check if is pressed the left arrow or A
+func IsPressedLeft(Input):
+	return (Input.is_action_pressed("ui_left") || Input.is_key_pressed(KEY_A));
+
+# Check if is pressed the down arrow or S
+func IsPressedDown(Input):
+	return (Input.is_action_pressed("ui_down") || Input.is_key_pressed(KEY_S));
+
+# Check if is pressed the up arrow or W
+func IsPressedUp(Input):
+	return (Input.is_action_pressed("ui_up") || Input.is_key_pressed(KEY_W));
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	# Vector2 is the player moement vecot
 	var velocity = Vector2();
 	# Input management
-	if Input.is_action_pressed("ui_right"):
+	if IsPressedRight(Input):
 		velocity.x += 1;
-	if Input.is_action_pressed("ui_left"):
+	if IsPressedLeft(Input):
 		velocity.x -= 1;
-	if Input.is_action_pressed("ui_down"):
+	if IsPressedDown(Input):
 		velocity.y += 1;
-	if Input.is_action_pressed("ui_up"):
+	if IsPressedUp(Input):
 		velocity.y -= 1;
 	if velocity.length() > 0:
 		# To avoid fast diagonal movement.
